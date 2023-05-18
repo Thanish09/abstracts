a4b8ca54ddb74704a047451843e075ba75f2ecbd
Sharvin1106 <sharvin.kogilavanan@gmail.com>
2023-05-18 20:57:13 +0800

    implement react query and refactor code

---
+++ src/components/Results/List.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ListItem, { Props as ListItemProps } from "./ListItem";
+import { EuiSpacer } from "@elastic/eui";
+
+type Props = {
+  items: ListItemProps[];
+};
+
+const List: React.FC<Props> = ({ items }) => {
+  return (
+    <>
+      {items?.map((result, index) => (
+        <>
+          <EuiSpacer />
+          <ListItem
+            key={index}
+            title={result.title}
+            abstract={result.abstract}
+            authors={result.authors}
+          />
+        </>
+      ))}
+      ;
+    </>
+  );
+};
+
+export default List;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import "@elastic/eui/dist/eui_theme_light.css";
 import { EuiProvider } from "@elastic/eui";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <EuiProvider colorMode="light">
-      <Component {...pageProps} />
-    </EuiProvider>
+    <QueryClientProvider client={queryClient}>
+      <EuiProvider colorMode="light">
+        <Component {...pageProps} />
+      </EuiProvider>
+    </QueryClientProvider>
   );
 }
