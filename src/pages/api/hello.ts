30f4f0a2c1c50344d069fd4fdfc581443bcad9b2
Sharvin1106 <sharvin.kogilavanan@gmail.com>
2023-05-23 22:13:51 +0800

    Included caching API

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,20 +1,39 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { redisClient } from "@/utils/redis";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { RedisClientOptions, RedisClientType } from "redis";
 
-type Data = {
-  name: string;
+type GetResponse = {
+  keyword: string;
+  found: boolean;
+  dateAdded?: string;
+};
+
+type PostResponse = {
+  message: string;
 };
 
 (async () => {
   await redisClient.connect();
 })();
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<GetResponse | PostResponse>
 ) {
-  const { suggest } = req.query;
-  res.status(200).json({ name: "John Doe" });
+  const suggest = req.query.suggest as string;
+  const keyword = req.body.keyword as string;
+
+  switch (req.method) {
+    case "GET":
+      const suggestVal = await redisClient.get(suggest);
+      if (!!suggestVal) {
+        return res
+          .status(200)
+          .json({ found: true, keyword: suggest, dateAdded: suggestVal });
+      }
+      return res.status(200).json({ found: false, keyword: suggest });
+    case "POST":
+      await redisClient.set(keyword, Date.now());
+      return res.status(201).json({ message: "Added" });
+  }
 }
